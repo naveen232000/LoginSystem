b3906bec405cf4a0401673d9d0462e1b4a51ae70
mygit@23 <naveenmadhesan23@gmail.com>
2024-02-19 13:07:18 +0530

    Add project files.

---
+++ LoginSystem/Authentication.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginSystem
+{
+    public  class Authentication
+    {
+        public string LogIn(string username, string password)
+        {
+            string lmsg ;
+                
+                if (string.IsNullOrEmpty(username)|| string.IsNullOrEmpty(password))
+            {
+                lmsg= "* Required,Pls Enter username and password";
+            }
+            else
+            { 
+                if((username == "Jon" && password == "Jon@23")||(username=="Admin" && password=="Admin@123")) 
+                
+                {
+                    lmsg = "Login Success !!!";
+                }
+                else
+                {
+                    lmsg = "Login Failure, pls enter valid username or password ";
+                }
+            }
+          return lmsg;
+        }
+    }
+}
---
+++ LoginSystem/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LoginSystem;
+Console.WriteLine("*** Welcome to Login System ***");
+try {
+  
+    Console.WriteLine("Enter UserName:");
+    string username = Console.ReadLine();
+    Console.WriteLine("Enter Password :");
+    string password = Console.ReadLine();
+    Authentication auth = new Authentication();
+    Console.WriteLine(auth.LogIn(username, password));
+}
+catch(Exception e) { Console.WriteLine(e.Message); }
